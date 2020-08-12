4c07e0facc930f75e4d0d2e3ccf53c36ca5ee2cb
7isenko <7isenko.max@gmail.com>
2020-08-12 13:04:06 +0300

    Replant dupe fix, moved api-version to 1.16.2

--- src/main/java/io/github/_7isenko/misc7/CropsReplant.java
+++ src/main/java/io/github/_7isenko/misc7/CropsReplant.java
@@ -9,12 +9,16 @@ import org.bukkit.block.data.Ageable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CropsReplant implements Listener {
+
     @EventHandler(ignoreCancelled = true)
     public void onRightClick(PlayerInteractEvent event) {
         if (event.getClickedBlock() == null && event.getAction() != Action.RIGHT_CLICK_BLOCK)
@@ -25,11 +29,23 @@ public class CropsReplant implements Listener {
         if (Tag.CROPS.isTagged(type) && !type.toString().contains("STEM")) {
             Ageable crop = (Ageable) block.getBlockData();
             if (crop.getAge() == crop.getMaximumAge()) {
-                block.breakNaturally();
+                Bukkit.getPluginManager().callEvent(new CropBreakEvent(block, event.getPlayer()));
                 block.getWorld().playSound(block.getLocation(), Sound.BLOCK_CROP_BREAK, 1, 1);
-                // welp, it duplicates seeds
-                block.setType(type);
+                crop.setAge(0);
+                block.setBlockData(crop);
             }
         }
     }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onCropBreak(CropBreakEvent event) {
+            Block crop = event.getBlock();
+            event.setDropItems(false);
+            ArrayList<ItemStack> drops = new ArrayList<>(crop.getDrops());
+            drops.get(drops.size() - 1).setAmount(drops.get(drops.size() - 1).getAmount() - 1);
+            for (ItemStack drop : drops) {
+                if (drop.getAmount() != 0)
+                crop.getWorld().dropItemNaturally(crop.getLocation(), drop);
+            }
+        }
 }
